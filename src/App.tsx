397ecf890ca0d81a974b393f91157be536360043
mandorakannu <mandorakannu@gmail.com>
2023-08-23 10:07:34 +0530

    TImeTable page created

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { Login } from "@components/pages/Login";
 import { FrontPage } from "@components/pages/main/FrontPage";
 import { Route, Routes } from "react-router-dom";
 import AOS from "aos";
-import 'aos/dist/aos.css';
+import "aos/dist/aos.css";
 import { Students } from "@components/pages/students/Students";
 import { Result } from "@components/pages/students/Result";
+import { TimeTable } from "@components/pages/students/TimeTable";
 
 export function App() {
   AOS.init({ once: true });
@@ -18,6 +19,7 @@ export function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/students" element={<Students />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/timeTable" element={<TimeTable />} />
       </Routes>
       <Footer />
     </>
---
+++ src/components/pages/students/TimeTable.tsx
@@ -0,0 +1,150 @@
+export function TimeTable() {
+  return (
+    <table className="dcf-table dcf-table-responsive dcf-table-bordered dcf-table-striped dcf-w-100% my-40">
+      <thead>
+        <tr>
+          <th className="dcf-txt-center" scope="col">
+            Days
+          </th>
+          <th className="dcf-txt-center" scope="col">
+            English
+          </th>
+          <th className="dcf-txt-center" scope="col">
+            Hindi
+          </th>
+          <th className="dcf-txt-center" scope="col">
+            Maths
+          </th>
+          <th className="dcf-txt-center" scope="col">
+            Science
+          </th>
+          <th className="dcf-txt-center" scope="col">
+            Computer
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th className="dcf-txt-center" scope="row">
+            Monday
+          </th>
+          <td className="dcf-txt-center" data-label="English">
+            9:00-10:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Hindi">
+            10:00-11:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Maths">
+            11:00-12:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Science">
+            12:00-1:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Computer">
+            1:00-2:00 PM
+          </td>
+        </tr>
+        <tr>
+          <th className="dcf-txt-center" scope="row">
+            Tuesday
+          </th>
+          <td className="dcf-txt-center" data-label="English">
+            9:00-10:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Hindi">
+            10:00-11:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Maths">
+            11:00-12:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Science">
+            12:00-1:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Computer">
+            1:00-2:00 PM
+          </td>
+        </tr>
+        <tr>
+          <th className="dcf-txt-center" scope="row">
+            Wednesday
+          </th>
+          <td className="dcf-txt-center" data-label="English">
+            9:00-10:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Hindi">
+            10:00-11:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Maths">
+            11:00-12:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Science">
+            12:00-1:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Computer">
+            1:00-2:00 PM
+          </td>
+        </tr>
+        <tr>
+          <th className="dcf-txt-center" scope="row">
+            Thursday
+          </th>
+          <td className="dcf-txt-center" data-label="English">
+            9:00-10:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Hindi">
+            10:00-11:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Maths">
+            11:00-12:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Science">
+            12:00-1:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Computer">
+            1:00-2:00 PM
+          </td>
+        </tr>
+        <tr>
+          <th className="dcf-txt-center" scope="row">
+            Friday
+          </th>
+          <td className="dcf-txt-center" data-label="English">
+            9:00-10:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Hindi">
+            10:00-11:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Maths">
+            11:00-12:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Science">
+            12:00-1:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Computer">
+            1:00-2:00 PM
+          </td>
+        </tr>
+        <tr>
+          <th className="dcf-txt-center" scope="row">
+            Saturday
+          </th>
+          <td className="dcf-txt-center" data-label="English">
+            9:00-10:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Hindi">
+            10:00-11:00 AM
+          </td>
+          <td className="dcf-txt-center" data-label="Maths">
+            11:00-12:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Science">
+            12:00-1:00 PM
+          </td>
+          <td className="dcf-txt-center" data-label="Computer">
+            1:00-2:00 PM
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
