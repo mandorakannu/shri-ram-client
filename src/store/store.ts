0ba88bd62d691133bbb412235da08e33f7f813b4
mandorakannu <mandorakannu@gmail.com>
2023-08-25 10:11:56 +0530

    Redux-persist added

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userSlice } from "@slices/user";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userSlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    user: userSlice.reducer,
+    user: persistedReducer,
   },
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
