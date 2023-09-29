c5c7c8fcb76864a7c0667fab9436f8ad4027835c
mandorakannu <mandorakannu@gmail.com>
2023-09-29 18:44:42 +0530

    Update: remove onChange fxn and now we are using useRef hook

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,34 +1,31 @@
 import { authUser } from "@functions/auth";
-import { FormEvent, useState, ChangeEvent, useRef } from "react";
+import { FormEvent, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { Loader } from "@components/Loader";
+
 export function Login() {
   const commanClass =
-    "m-3 px-5 py-2 rounded bg-white outline outline-2 linkedin hover:text-white hover:bg-blue-500 w-64";
-  const [user, setUser] = useState({
-    name: "",
-    password: "",
-    uniqueId: "",
-  });
+    "m-3 px-5 py-2 rounded bg-white outline outline-2 linkedin hover:text-white hover:bg-blue-500 w-64"; //? Common class for 3 buttons (Student, Teacher, Admin)
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const uniqueIdRef = useRef<HTMLInputElement>(null);
   const invalidTagRef = useRef<HTMLHeadingElement>(null);
-  const [login, setLogin] = useState("STUDENT");
-  const [loader, setLoader] = useState<JSX.Element | "Login">("Login");
+  const [login, setLogin] = useState<"STUDENT" | "TEACHER" | "ADMIN">(
+    "STUDENT"
+  );
+  const [loader, setLoader] = useState<JSX.Element | "Login">("Login"); //? Loader for login button
   const navigate = useNavigate();
-  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const invalidTag = document.querySelector("#invalidTag") as HTMLElement;
-    if (invalidTag) {
-      invalidTag.style.visibility = "hidden";
-      setUser({ ...user, [e.target.name]: e.target.value });
-    } else {
-      setUser({ ...user, [e.target.name]: e.target.value });
-    }
-  };
 
+  //! Login Handler function
   const onSubmitHandler = async (
     event: FormEvent<HTMLFormElement>
   ): Promise<void | never> => {
     event.preventDefault();
-    if (user.name === "" || user.password === "" || user.uniqueId === "") {
+    const name = usernameRef.current?.value as string;
+    const password = passwordRef.current?.value as string;
+    const uniqueId = uniqueIdRef.current?.value as string;
+
+    if (name === "" || password === "" || uniqueId === "") {
       if (invalidTagRef.current) {
         invalidTagRef.current.style.visibility = "visible";
         invalidTagRef.current.innerText = "Fill All Fields";
@@ -36,7 +33,11 @@ export function Login() {
       return;
     } else {
       setLoader(<Loader barColor="blue" wrapperStyle={{ scale: "2.0" }} />);
-      const isAuth = await authUser(login.toLowerCase(), user);
+      const isAuth = await authUser(login.toLowerCase(), {
+        name,
+        password,
+        uniqueId,
+      });
       if (isAuth.message === "user authenticated") {
         if (isAuth.role === "student") return navigate("/students");
         else if (isAuth.role === "teacher") return navigate("/teachers");
@@ -44,7 +45,6 @@ export function Login() {
         else {
           if (invalidTagRef.current) {
             invalidTagRef.current.style.visibility = "visible";
-            invalidTagRef.current.innerText = "Fill All Fields";
             invalidTagRef.current.innerText = "Something went wrong!";
           }
         }
@@ -73,7 +73,7 @@ export function Login() {
       >
         <h1 className="font-bold text-xl">{login} LOGIN</h1>
         <h2
-          className="red bg-red-100 w-64 text-center py-1 my-1 rounded invisible"
+          className="bg-red-100 w-64 text-center py-1 my-1 rounded invisible"
           id="invalidTag"
           ref={invalidTagRef}
         >
@@ -82,26 +82,23 @@ export function Login() {
         <input
           type="text"
           name="name"
-          value={user.name}
           placeholder="Name"
           className="bg-gray-200 px-3 py-2 rounded my-3 w-64"
-          onChange={onChangeHandler}
+          ref={usernameRef}
         />
         <input
           type="password"
           name="password"
-          value={user.password}
           placeholder="Password"
           className="bg-gray-200 px-3 py-2 rounded my-3 w-64"
-          onChange={onChangeHandler}
+          ref={passwordRef}
         />
         <input
           type="password"
           name="uniqueId"
-          value={user.uniqueId}
           placeholder="Unique Id"
           className="bg-gray-200 px-3 py-2 rounded my-3 w-64"
-          onChange={onChangeHandler}
+          ref={uniqueIdRef}
         />
 
         <button
